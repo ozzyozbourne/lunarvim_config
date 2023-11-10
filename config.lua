62c99aa8d61748248c9fca061ee1164c58b5b9ed
ozzy ozbourne <khanosaid726@gmail.com>
2023-11-11 00:47:06 +0530

    lunar vim config

---
+++ config.lua
@@ -0,0 +1,76 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+lvim.plugins = {
+  "mfussenegger/nvim-jdtls",
+  {"rose-pine/neovim" , name = "rose-pine"}, 
+}
+
+-- theme
+require('rose-pine').setup({
+    --- @usage 'auto'|'main'|'moon'|'dawn'
+    variant = 'dawn',
+    --- @usage 'main'|'moon'|'dawn'
+    dark_variant = 'main',
+    bold_vert_split = false,
+    dim_nc_background = false,
+    disable_background = false,
+    disable_float_background = false,
+    disable_italics = false,
+
+    --- @usage string hex value or named color from rosepinetheme.com/palette
+    groups = {
+        background = 'base',
+        background_nc = '_experimental_nc',
+        panel = 'surface',
+        panel_nc = 'base',
+        border = 'highlight_med',
+        comment = 'muted',
+        link = 'iris',
+        punctuation = 'subtle',
+
+        error = 'love',
+        hint = 'iris',
+        info = 'foam',
+        warn = 'gold',
+
+        headings = {
+            h1 = 'iris',
+            h2 = 'foam',
+            h3 = 'rose',
+            h4 = 'gold',
+            h5 = 'pine',
+            h6 = 'foam',
+        }
+        -- or set all headings at once
+        -- headings = 'subtle'
+    },
+
+    -- Change specific vim highlight groups
+    -- https://github.com/rose-pine/neovim/wiki/Recipes
+    highlight_groups = {
+        ColorColumn = { bg = 'rose' },
+
+        -- Blend colours against the "base" background
+        CursorLine = { bg = 'foam', blend = 10 },
+        StatusLine = { fg = 'love', bg = 'love', blend = 10 },
+
+        -- By default each group adds to the existing config.
+        -- If you only want to set what is written in this config exactly,
+        -- you can set the inherit option:
+        Search = { bg = 'gold', inherit = false },
+    }
+})
+
+
+lvim.colorscheme = "rose-pine"
+lvim.builtin.lualine.style = "default"
+
+-- Java LSP and debugger settings
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "jdtls" })
+
+lvim.builtin.treesitter.ensure_installed = {
+  "java",
+}
